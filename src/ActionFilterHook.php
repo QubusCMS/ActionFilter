3f49d55f7cd8f57e51ddd9d59352c719b1cc0fff
Joshua Parker <josh@joshuaparker.blog>
2020-01-13 22:56:35 -0700

    Added trait library.

--- src/ActionFilterHook.php
+++ src/ActionFilterHook.php
@@ -20,7 +20,9 @@ final class ActionFilterHook implements
     HasFilterHookInterface,
     HasActionHookInterface
 {
-    const VERSION = '1.0.0';
+    use \Qubus\Traits\SingletonTrait;
+
+    const VERSION = '1.0.1';
     /**
      *
      * @access protected
@@ -64,37 +66,6 @@ final class ActionFilterHook implements
      * @const int
      */
     protected const ARGUMENT_NEUTRAL = 1;
-
-    /**
-     * @access protected
-     * @var self The stored singleton instance
-     */
-    protected static $instance;
-
-    /**
-     * Reset the Container instance.
-     */
-    public static function resetInstance()
-    {
-        if (self::$instance) {
-            self::$instance = null;
-        }
-    }
-
-    /**
-     * Creates the original or retrieves the stored singleton instance
-     *
-     * @return self
-     */
-    public static function getInstance()
-    {
-        if (!static::$instance) {
-            static::$instance = (new \ReflectionClass(get_called_class()))
-                ->newInstanceWithoutConstructor();
-        }
-
-        return static::$instance;
-    }
 
     /**
      * Add a new action to the collection to be registered with Qubus CMS.
